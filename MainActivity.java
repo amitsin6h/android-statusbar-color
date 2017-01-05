df932e4aab206acea34fcc3907ebdfc8019448ba
Amit singh <amitsin6h@users.noreply.github.com>
2017-01-05 16:57:19 +0530

    Create MainActivity.java
    add the add code inside the onCreate Method .
    But this will only work on Android Lollipo(API 21) and higher

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+//change statusbar color
+
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorMain));
+        }
+        
+       //end
+        
+        }
+        }
